89eb01d470e53fa044b229cccf897457c33b2b45
SK-Sarkar-Programmer <shawonkumarsarkar.developer@gmail.com>
2025-07-11 14:32:26 +0600

    Marquee complete

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-document.addEventListener("DOMContentLoaded", function () {});
+document.addEventListener("DOMContentLoaded", function () {
+  const marquees = document.querySelectorAll(".marquee-container");
+
+  if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+    marqueeAnimation();
+  }
+
+  function marqueeAnimation() {
+    marquees.forEach((marquee) => {
+      marquee.setAttribute("marquee-animation", true);
+    });
+
+    const content = document.querySelector(".marquee-content");
+    const contentCopies = Array.from(content.children);
+
+    contentCopies.forEach((child) => {
+      const copies = child.cloneNode(true);
+      copies.setAttribute("aria-hidden", true);
+
+      content.appendChild(copies);
+    });
+  }
+});
